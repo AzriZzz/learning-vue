f7fc709c551590a90d31aec2b572f647d7abef34
Muhammad Azri <lenovorosak@gmail.com>
2022-01-11 23:58:07 +0800

    Lesson: Modal and event-handling - close

--- main.js
+++ main.js
@@ -18,6 +18,28 @@ Vue.component('message', {
   `,
 });
 
+Vue.component('modal', {
+  props: ['body'],
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <p>
+            {{ body }}
+            <slot></slot>
+          </p>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+  `,
+});
+
 new Vue({
-  el: '#root'
+  el: '#root',
+
+  data: {
+    showModal: false
+  }
 })

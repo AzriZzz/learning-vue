95ccda74dcf0429a65f981228cb8a9ea31dc94cc
Muhammad Azri <lenovorosak@gmail.com>
2022-01-12 22:10:43 +0800

    Lesson: Tabs and dynamic content, with binding href

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-Vue.component('message', {
-  props: ['title', 'body'],
+Vue.component("message", {
+  props: ["title", "body"],
   data() {
-    return{
-      isVisible: true
-    }
+    return {
+      isVisible: true,
+    };
   },
   template: `
     <article class="message" v-show='isVisible'>
@@ -18,8 +18,8 @@ Vue.component('message', {
   `,
 });
 
-Vue.component('modal', {
-  props: ['body'],
+Vue.component("modal", {
+  props: ["body"],
   template: `
     <div class="modal is-active">
       <div class="modal-background"></div>
@@ -36,10 +36,67 @@ Vue.component('modal', {
   `,
 });
 
+Vue.component("tabs", {
+  template: `
+    <div>
+        <div class="tabs">
+          <ul>
+            <li v-for='tab in tabs' :class="{ 'is-active': tab.isActive}">
+              <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="tabs-details">
+          <slot></slot>
+        </div>
+    </div>
+  `,
+  data() {
+    return { tabs: [] };
+  },
+  created() {
+    console.log(this.$children);
+    this.tabs = this.$children;
+  },
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach( tab => {
+        tab.isActive = (tab.name === selectedTab.name);
+      })
+    }
+  }
+});
+
+Vue.component("tab", {
+  template: `
+    <div v-show="isActive">
+      <slot></slot>
+    </div>
+  `,
+  props: {
+    name: { required: true },
+    selected: { default: false}
+  },
+  computed: {
+    href(){
+      return '#' + this.name.toLowerCase().replace(/ /g, '-');
+    }
+  },
+  data() {
+    return{
+      isActive: false
+    }
+  },
+  mounted() {
+    this.isActive = this.selected
+  },
+});
+
 new Vue({
-  el: '#root',
+  el: "#root",
 
   data: {
-    showModal: false
-  }
-})
+    showModal: false,
+  },
+});
